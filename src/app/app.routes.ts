87ab5fd1898363d28270f0d04a5267969a7945a9
Emma Rosso <emmarosso.pro@gmail.com>
2023-05-25 17:28:52 +0200

    add disabled host check

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { MenuComponent } from './menu/menu.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 
