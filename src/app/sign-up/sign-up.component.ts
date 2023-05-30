80d9704b32091fd0acf5070ae6cb037a53b62a24
Emma Rosso <emmarosso.pro@gmail.com>
2023-05-30 12:49:07 +0200

    added a signup form

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from "./user";
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent {
+model: User = new User("", "", "", "")
 
+onSubmit = () => {
+  console.log(this.model)
+}
 }
